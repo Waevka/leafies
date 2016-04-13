94aed9f5930b1c77d178841fb193cb0660acc904
Waevka <waveswallow@gmail.com>
2016-04-13 17:38:37 +0200

    Nodes are now properly created. Wip winner leaf.

--- fifteen/main.cpp
+++ fifteen/main.cpp
@@ -15,33 +15,43 @@ struct Listek {
 	string solution;
 };
 void initialize(Listek *l);
+void tabCopy(int a[], int b[]);
 void draw(Listek *l);
 void randomujTo(int ile, Listek *l);
 bool check(int pos, dir d);
 void swapKappa(int tab[], int pos, dir d);
-void reverseDir(dir d, dir *lastd);
+dir reverseDir(dir d);
 dir returnLastDir(string s);
 void writeLetter(string *s, dir d);
-bool dawajBFS(Listek *begin, int depth);
+bool dawajBFS(Listek *begin, int depth, Listek *winner);
 bool checkIfFinished(int tab[]);
 void generateMoves(std::queue <Listek> *q);
+Listek *makeANode(Listek last, dir d);
+Listek *copyLeaf(Listek c);
+void drawNodeInfo(Listek l);
 
 /////////////////////////////////////
 int main()
 {
 	srand(time(NULL));
 	Listek *first = new Listek();
+	Listek *winner = new Listek();
 
 	initialize(first);
+	cout << "\n\n CASE 1: ------------------------------------\n";
 	draw(first);
 	cout << endl << "\nValid tabliczka: " << checkIfFinished(first->tab);
-	cout << "\nzagadka jest rozwiazywalna? : " << dawajBFS(first, 1);
+	cout << "\nzagadka jest rozwiazywalna? : " << dawajBFS(first, 1, winner);
+	cout << "\n\n The winner leaf: \n";
+	draw(winner);
+	cout << "\n\n CASE 2: ------------------------------------\n";
 	randomujTo(1, first);
 	cout << endl;
 	draw(first);
 	cout << endl << "\nValid tabliczka: " << checkIfFinished(first->tab);
-	cout << "\nzagadka jest rozwiazywalna? : " << dawajBFS(first, 1);
-
+	cout << "\nzagadka jest rozwiazywalna? : " << dawajBFS(first, 2, winner);
+	cout << "\n\n The winner leaf: \n";
+	draw(winner);
 	cin.get();
 	return 0;
 }
@@ -61,6 +71,13 @@ void initialize(Listek *l) {
 			l->tab[i] = 0;
 			l->zeroPos = N*N - 1;
 		}
+	}
+}
+
+void tabCopy(int a[], int b[]) {
+	//copies from b to a
+	for (int i = 0; i < N*N; i++) {
+		a[i] = b[i];
 	}
 }
 
@@ -88,9 +105,8 @@ void randomujTo(int ile, Listek *l) {
 		else { d = UP; }
 
 		if (check(l->zeroPos, d) && d != lastd) {
-			writeLetter(&(l->solution), d);
 			swapKappa(l->tab, l->zeroPos, d);
-			reverseDir(d, &lastd);
+			lastd = reverseDir(d);
 			l->zeroPos += d;
 			count++;
 		}
@@ -134,24 +150,26 @@ void swapKappa(int tab[], int pos, dir d) {
 }
 
 //Reverses the position of d, since the enum holds values that cannot be changed in a simpler way
-void reverseDir(dir d, dir *lastd) {
+dir reverseDir(dir d) {
+	dir lastd = NONE;
 	switch (d)
 	{
 	case UP:
-		*lastd = DOWN;
+		lastd = DOWN;
 		break;
 	case DOWN:
-		*lastd = UP;
+		lastd = UP;
 		break;
 	case LEFT:
-		*lastd = RIGHT;
+		lastd = RIGHT;
 		break;
 	case RIGHT:
-		*lastd = LEFT;
+		lastd = LEFT;
 		break;
 	default:
 		break;
 	}
+	return lastd;
 }
 
 dir returnLastDir(string s) {
@@ -194,27 +212,27 @@ void writeLetter(string *s, dir d) {
 	}
 }
 
-bool dawajBFS(Listek *begin, int depth) {
+bool dawajBFS(Listek *begin, int depth, Listek *winner) {
 	std::queue <Listek> *bingo = new queue <Listek>();
 	bingo->push(*begin);
+	drawNodeInfo(*begin);
 	bool finished = false;
 	bool solvable = false;
 
-	while (!bingo->empty() && finished != true) {
+	while (!bingo->empty() && finished == false) {
 		if (checkIfFinished(bingo->front().tab)) {
 			finished = true;
 			solvable = true;
+			winner = copyLeaf(bingo->front());
 			cout << "\nqueue finished with success";
-		}
-		if (bingo->front().depth == depth) {
+		}	else if (bingo->front().depth + 1 > depth) {
 			finished = true;
-		} else {
+		}	else {
 			// time to add sum leaves
 			generateMoves(bingo);
+			bingo->pop();
 		}
-		bingo->pop();
 	}
-
 	return solvable;
 }
 
@@ -240,16 +258,64 @@ bool checkIfFinished(int tab[]) {
 void generateMoves(std::queue <Listek> *q) {
 	Listek last = q->front();
 	dir lastdir = returnLastDir(last.solution);
-	if (check(last.zeroPos, UP) && lastdir != UP) {
+	lastdir = reverseDir(lastdir);
 
+	if (check(last.zeroPos, UP) && lastdir != UP) {
+		Listek *pushMe;
+		cout << "\nUP possible";
+		pushMe = makeANode(last, UP);
+		q->push(*pushMe);
+		drawNodeInfo(*pushMe);
 	}
 	if (check(last.zeroPos, DOWN) && lastdir != DOWN) {
-
+		Listek *pushMe;
+		cout << "\nDOWN possible";
+		pushMe = makeANode(last, DOWN);
+		q->push(*pushMe);
+		drawNodeInfo(*pushMe);
 	}
 	if (check(last.zeroPos, LEFT) && lastdir != LEFT) {
-
+		Listek *pushMe;
+		cout << "\nLEFT possible";
+		pushMe = makeANode(last, LEFT);
+		q->push(*pushMe);
+		drawNodeInfo(*pushMe);
 	}
 	if (check(last.zeroPos, RIGHT) && lastdir != RIGHT){
-
+		Listek *pushMe;
+		cout << "\nRIGHT possible";
+		pushMe = makeANode(last, RIGHT);
+		q->push(*pushMe);
+		drawNodeInfo(*pushMe);
 	}
+}
+
+Listek *makeANode(Listek last, dir d) {
+	Listek *l = new Listek();
+	tabCopy(l->tab, last.tab);
+	l->depth = last.depth + 1;
+	l->solution = last.solution;
+	l->zeroPos = last.zeroPos;
+	writeLetter(&(l->solution), d);
+	swapKappa(l->tab, l->zeroPos, d);
+	l->zeroPos += d;
+	return l;
+}
+
+Listek *copyLeaf(Listek c) {
+	Listek *l = new Listek();
+	tabCopy(l->tab, c.tab);
+	l->depth = c.depth;
+	l->solution = c.solution;
+	l->zeroPos = c.zeroPos;
+	return l;
+}
+
+void drawNodeInfo(Listek l) {
+	cout << "\n===========";
+	cout << "\n|Depth:" << l.depth << " |";
+	cout << "\n|Zeropos:" << l.zeroPos << " |";
+	cout << "\n|Solution: " << l.solution << " |" << endl;
+	draw(&l);
+	cout << "\n===========";
 }
